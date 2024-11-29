b686ad748b7886d81581e17a322014e1afad4ba8
Hiteshkumar00 <hiteshkumar252020@gmail.com>
2024-11-29 19:22:13 +0530

    new feature

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const app = express();
 const path = require("path");
 const fs = require('fs');
 
+const session = require('express-session');
+const flash = require('connect-flash');
+
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
+
+app.use(session({
+  secret: 'notUsefull',
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(flash());
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -16,8 +26,8 @@ const upload = multer({ dest: 'uploads/' });
 
 const send = require("./sendmail");
 
-app.get("/mail", (req, res) => {
-  res.render("index.ejs")
+app.get("/mail", (req, res) => {  
+  res.render("index.ejs", { message : req.flash('info')[0] })
 });
 
 app.post("/send", upload.array('data', 10), async (req, res) => {
@@ -25,7 +35,8 @@ app.post("/send", upload.array('data', 10), async (req, res) => {
   let data = req.files;
   data.forEach((f, i) => {data[i] = {filename: f.originalname, path : f.path}});
   await send(req.body.to, req.body.sub, req.body.msg, data).catch((err) => console.log(err));
-  data.forEach((f) => {fs.unlinkSync(f.path);})
+  data.forEach((f) => {fs.unlinkSync(f.path)});
+  req.flash('info', `${req.body.to}`);
   res.redirect("/mail");
 });
 
--- public/index.js
+++ public/index.js
@@ -11,4 +11,25 @@ function addInputFile(){
   newFile.value = "";
   let fileDiv = document.getElementById('files');
   fileDiv.appendChild(newFile);
-}
+};
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})();
